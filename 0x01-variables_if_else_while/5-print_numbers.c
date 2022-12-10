c21955bcc5cccf0e30068dccc865d9a2c99739e8
David Tei <daveteidt@gmail.com>
2022-12-10 07:13:11 +0000

    Printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * printing numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int t;
+
+	for (t = 0; t <= 9; t++)
+	{
+		printf("%d", t);
+	}
+	printf("\n");
+	return (0);
+}
