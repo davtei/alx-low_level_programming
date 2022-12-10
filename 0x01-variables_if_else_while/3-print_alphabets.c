a08c0900d4cb864331500968d507f50b5b4cd7d4
David Tei <daveteidt@gmail.com>
2022-12-10 06:11:36 +0000

    List a-zA-Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * printing alphabets from a to z and then A to Z with putchar()
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+{
+	putchar(a);
+}
+	for (a = 'A'; a <= 'Z'; a++)
+{
+	putchar(a);
+}
+	return (0);
+}
