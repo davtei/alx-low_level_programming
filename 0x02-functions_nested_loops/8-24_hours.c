49ab7ea3e96775b97eaba1f07176b2177835948c
David Tei <daveteidt@gmail.com>
2022-12-14 23:23:50 +0000

    Jack Bauer 24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - Function to create a Jack Bauer timer,
+ * from 00:00 to 23:59
+ *
+ * return: 0
+ */
+void jack_bauer(void)
+{
+	int hr = 0;
+	int min = 0;
+
+	if (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
