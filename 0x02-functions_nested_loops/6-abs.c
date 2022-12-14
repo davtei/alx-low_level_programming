14be31b4870c2524ef96c2f57549c7cf0719e389
David Tei <daveteidt@gmail.com>
2022-12-14 05:44:28 +0000

    Function to print absolutes

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an integer.
+ *
+ * Return: Always 0
+ */
+int _abs(int)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (-a);
+	}
+}
