5997de3d87223c390900c7616cefc124d4b563fa
David Tei <daveteidt@gmail.com>
2022-12-14 05:45:58 +0000

    Function to print absolutes

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,9 +2,11 @@
 /**
  * _abs - function that computes the absolute value of an integer.
  *
+ *@a: positive or negative integer supplied.
+ *
  * Return: Always 0
  */
-int _abs(int)
+int _abs(int a)
 {
 	if (a >= 0)
 	{
