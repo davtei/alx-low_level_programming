8e57af71fd40050c63786ad444072dd9588d52ca
David Tei <daveteidt@gmail.com>
2022-12-14 01:26:11 +0000

    Added fxn to print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - function to print the alphabet in lower case,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar('x');
+	}
+	_putchar('\n');
+}
