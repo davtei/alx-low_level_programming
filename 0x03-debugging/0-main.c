ba867dae9a7560e74db80f26251b54656f6a5823
David Tei <daveteidt@gmail.com>
2022-12-15 16:38:01 +0000

    Debugging function checker 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
  * main - test function that prints if integer is positive or negative
- *
- * Return: Always 0
+ * Return: 0
  */
+
 int main(void)
 {
-	int t;
+	int i;
 
 	i = 0;
 	positive_or_negative(i);
