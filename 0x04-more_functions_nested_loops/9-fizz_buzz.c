1bc4b33346aa9646a2e28baccf4ba2abf1cf68b7
David Tei <daveteidt@gmail.com>
2022-12-16 16:20:47 +0000

    FizzBuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * main - Fizz-Buzz test
+ * Return: void
+ */
+int main(void)
+{
+	int f = 1;
+
+	while (f <= 100)
+	{
+		if (f % 3 == 0 && f % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (f % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (f % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%f", f);
+		}
+
+		if (f != 100)
+		{
+			putchar(' ');
+		}
+		f++
+	}
+	putchar('\n');
+	return (0);
+}
