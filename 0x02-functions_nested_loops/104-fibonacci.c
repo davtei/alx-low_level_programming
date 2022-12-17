6dddd5f30f35a500f7cbbaa512191ff88ba72af7
David Tei <daveteidt@gmail.com>
2022-12-17 01:52:23 +0000

    Fibonacci sequence 3

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - program the prints the first 90 terms in the Fibonacci
+ * sequence, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int t = 0;
+	unsigned long int a = 0, b = 1, f = 0;
+
+	while (t < 100)
+	{
+		f = a + b;
+		a = b;
+		b = t;
+		printf("%lu", f);
+
+		if (t < 97)
+		{
+			printf(", ");
+		}
+		t++;
+	}
+	putchar('\n');
+	return (0);
+}
