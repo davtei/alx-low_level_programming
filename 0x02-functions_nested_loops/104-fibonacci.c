6dc7dae6d88e1b92f902247ff169cd0f7947a64f
David Tei <daveteidt@gmail.com>
2022-12-18 12:31:22 +0000

    Fibonacci sequence 3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,22 +6,43 @@
  */
 int main(void)
 {
-	int t = 0;
-	unsigned long int a = 0, b = 1, f = 0;
+	int n;
+	unsigned long one = 0, two = 1, fib;
+	unsigned long one_half1, one_half2, two_half1, two_half2;
+	unsigned long half1, half2;
 
-	while (t < 98)
+	for (n = 0; n < 92; n++)
 	{
-		f = a + b;
-		a = b;
-		b = f;
-		printf("%lu", f);
+		fib = one + two;
+		printf("%lu, ", fib);
 
-		if (t < 97)
-		{
-			printf(", ");
-		}
-		t++;
+		one = two;
+		two = fib;
 	}
-	putchar('\n');
+
+	one_half1 = one / 10000000000;
+	two_half1 = two / 10000000000;
+	one_half2 = one % 10000000000;
+	two_half2 = two % 10000000000;
+
+	for (n = 93; n < 99; n++)
+	{
+		half1 = one_half1 + two_half1;
+		half2 = one_half2 + two_half2;
+		if (one_half2 + two_half2 > 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+		printf("%lu%lu", half1, half2);
+		if (n != 98)
+			printf(", ");
+
+		one_half1 = two_half1;
+		one_half2 = two_half2;
+		two_half1 = half1;
+		two_half2 = half2;
+	}
+	printf("\n");
 	return (0);
 }
