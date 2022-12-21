dcdaaa1a3c8bbfe616c417390350c2b7ad465bd8
David Tei <daveteidt@gmail.com>
2022-12-21 23:51:14 +0000

    Added _strncat function

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * _strcat - function that appends the src string to the dest string,
+ * _strncat - function that appends n bytes of the src string to the dest string,
  * overwriting the terminating null byte (\0) at the end of dest,
- * and then adds a terminating null byte.
  * @dest: destination string
  * @src: source string
+ * @n: number of bytes from src string
  * Return: a pointer to the resulting string dest.
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int len1 = 0, len2 = 0, i = 0;
+	int len1 = 0, i = 0;
 
 	while (dest[len1] != '\0')
 	{
 		len1++;
 	}
-	while (src[len2] != '\0')
+	/*while (src[len2] != n)
 	{
 		len2++;
-	}
-	while (i <= len2)
+	}*/
+	while (i < n && src[i] != '\0')
 	{
 		dest[len1 + i] = src[i];
 		i++;
