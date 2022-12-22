5177c38142579afcd01c446c43bc0917ab4c8cbf
David Tei <daveteidt@gmail.com>
2022-12-22 10:55:39 +0000

    Added rot13 function

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13.
+ * @str: string to encode.
+ * Return: encoded string.
+ */
+char *rot13(char *str)
+{
+	char org[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	int i = 0;
+	int a;
+
+	while (*(str + i) != '\0')
+	{
+		for (a = 0; a <= 51; a++)
+		{
+			if (*(str + i) == org[a])
+			{
+				*(str + i) = rot[a];
+				break;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
+		
