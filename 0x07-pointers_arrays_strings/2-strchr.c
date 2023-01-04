3661716bdc0fe0ac687e4c732d368d5934a0fbb9
David Tei <daveteidt@gmail.com>
2023-01-04 05:39:57 +0000

    Added strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @c: search query
+ * @s: string to search
+ * Return: pointer to c or NULL.
+ */
+*_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			retun (s +);
+		}
+	}
+	if (*(s + i) == c)
+	{
+		return ('\0');
+	}
