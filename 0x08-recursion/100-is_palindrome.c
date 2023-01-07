ba25f2f4e38af4a5d48632bb459ddd3850f9d590
David Tei <daveteidt@gmail.com>
2023-01-07 02:21:06 +0000

    Added is_palindrome function

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
 /**
- * str_len - function that calculates the length of a string
+ * _strlen_recursion - function that calculates the length of a string
  * @s: string
  * Return: length of a string
  */
-int str_len(char *s)
+int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*(s + len))
-		len++;
-	len += str_len(s + len);
-	return (len);
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
 }
 /**
  * chck_pal - function that checks if a string is a palindrome.
  * @s: string
- * @len: length of string s
- * @i: iterator or index of string to be checked.
+ * @i: iterator or index of string to the left of median.
+ * @j: iterator or index of string to the right of median.
  * Return: 1 if s is a palindrome, otherwise 0.
  */
-int chck_pal(char *s, int len, int i)
+int chck_pal(char *s, int i, int j)
 {
-	if (s[i] == s[len / 2])
-		return (1);
-	if (s[i] == s[len - i - 1])
-		return (chck_pal(s, len, i + 1));
-	return (0);
+	if (s[i] == s[j])
+		if (i > j / 2)
+			return (1);
+		else
+			return (chck_pal(s, i + 1, j - 1));
+	else
+		return (0);
 }
 /**
  * is_palindrome - function that checks if a string is a palindrome.
@@ -36,10 +36,5 @@ int chck_pal(char *s, int len, int i)
  */
 int is_palindrome(char *s)
 {
-	int i = 0;
-	int len = str_len(s);
-
-	if (!(*s))
-		return (1);
-	return (chck_pal(s, len, i));
+	return (chck_pal(s, 0, _strlen_recursion(s) - 1));
 }
