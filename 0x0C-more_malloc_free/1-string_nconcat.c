ae08a4d13945208e32f0dfaca5945bfa83de9b68
David Tei <daveteidt@gmail.ocm>
2023-01-12 22:04:33 -0800

    Added string_nconcat function

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,9 +32,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (n > len2)
 		n = len2;
-	p = malloc((len1 + n + 1) * sizeof(char));
+	p = malloc((len1 + len2 + 1) * sizeof(char));
 	if (p == NULL)
-		return (0);
+		return (NULL);
 	for (i = 0; i < len1; i++)
 	{
 		p[i] = s1[i];
