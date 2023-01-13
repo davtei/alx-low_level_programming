8b02925eab0e96aac0903c37dfc4ab03e88e4d88
David Tei <daveteidt@gmail.ocm>
2023-01-13 12:52:32 -0800

    Added array_range function

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -6,6 +6,8 @@
  * The array created contains all the values from min (included) to
  * max (included), ordered from min to max.
  * If min > max, return NULL.
+ * @min: minimum value.
+ * @max: maximum value.
  * Return: the pointer to the newly created array (Success), otherwise NULL.
  */
 int *array_range(int min, int max)
