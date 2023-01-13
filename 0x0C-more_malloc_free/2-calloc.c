25d67e7fdafb0a1f0e3d808eff2ad879153fbe08
David Tei <daveteidt@gmail.ocm>
2023-01-13 13:04:09 -0800

    Added calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	c = malloc(nmemb * size)
+	c = malloc(nmemb * size);
 	if (c == NULL)
 		return (NULL);
 	for (a = 0; a < (nmemb * size); a++)
