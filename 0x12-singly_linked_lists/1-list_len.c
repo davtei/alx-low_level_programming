d7e1739a06f5ff316cfb9155cd9324404cf4c9f7
David Tei <daveteidt@gmail.com>
2023-01-27 01:52:17 +0000

    Added list_len function

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements
+ * in a linked list_t list.
+ * @h: head of the linked list.
+ * Return: number of elements.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t size;
+
+	for (size = 0; h != NULL; size++)
+	{
+		h = h->next;
+	}
+	return (size);
+}
